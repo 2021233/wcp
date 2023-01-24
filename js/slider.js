aed21f6821392c18b7df9c53565c1eea67653c6b
negi <eadxNM297@gmail.com>
2023-01-24 21:02:55 +0900

    edit shop page
    edit slider
    edit recipe section at introduction page

--- js/slider.js
+++ js/slider.js
@@ -1,47 +1,61 @@
-let id = 0;
-let pages = document.querySelectorAll('.page');
-let dots = document.querySelectorAll('.page-dot');
+if (typeof id == 'undefinded') { let id = 0; }
+else { id = 0 };
+if (typeof pages == 'undefinded') { let pages = document.querySelectorAll('.page'); }
+else { pages = document.querySelectorAll('.page'); };
+if (typeof dots == 'undefinded') { let dots = document.querySelectorAll('.page-dot'); }
+else { dots = document.querySelectorAll('.page-dot'); };
 
-let prev = document.querySelector('.prev');
-let next = document.querySelector('.next')
+if (typeof prev == 'undefinded') { let prev = document.querySelector('.prev'); }
+else { prev = document.querySelector('.prev'); };
+if (typeof next == 'undefinded') { let next = document.querySelector('.next') }
+else { next = document.querySelector('.next') };
 
-const paging = () => {
+
+function paging() {
     for (let j = 0; j < dots.length; j++) {
         dots[j].firstElementChild.removeAttribute('class');
     }
     dots[id].firstElementChild.setAttribute('class', 'current');
+    console.log('paging : ' + id);
 }
 
-const page_switch = () => {
-    for (let j = 0; j < pages.length; j++){
+function page_switch() {
+    for (let j = 0; j < pages.length; j++) {
         pages[j].classList.add('hidden');
     }
-    
     pages[id].classList.remove('hidden');
+    if(pages.length > dots.length) { 
+        pages[pages.length/2 + id].classList.remove('hidden');
+    }
+    console.log('page_switch : ' + id);
 }
 
-const prev_paging = () => {
+function prev_paging() {
     id--;
-    if(id <= 0) {
+    if (id <= 0) {
         id = 0;
         prev.classList.add('hidden');
     }
-    if(id == pages.length - 2) next.classList.remove('hidden');
+    if (id == dots.length - 2) next.classList.remove('hidden');
     paging();
     page_switch();
+    console.log('prev_paging : ' + id);
 }
 
-const next_paging = () => {
+function next_paging() {
     id++;
-    if(id >= pages.length -1) {
-        id = pages.length - 1;
+    if (id >= dots.length - 1) {
+        id = dots.length - 1;
         next.classList.add('hidden');
     }
-    if(id == 1) prev.classList.remove('hidden');
+    if (id == 1) prev.classList.remove('hidden');
     paging();
     page_switch();
+    console.log('next_paging : ' + id);
 }
 
 prev.addEventListener('click', prev_paging);
 
 next.addEventListener('click', next_paging);
+
+console.log('loaded : ' + id);
